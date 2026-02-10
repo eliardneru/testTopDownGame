452d6e1f11a3b1c7cc0f58ebdbfa36e8e26f5aa8
eliardneru <eliardnotneru@gmail.com>
2026-02-10 18:13:13 -0300

    imgui and decent colision

--- src/main.cpp
+++ src/main.cpp
@@ -1,95 +1,137 @@
-#include "raylib.h"
-#include <locale>
 #include <stdio.h>
+#include <math.h>
 
+#include "raylib.h"
+#include "rlImGui.h"
+#include "imgui/imgui.h"
+
+//"ehh, good enough" - mediocrates
+
+//basic 720, probably won't be changed in the future
 int HEIGHT = 720;
 int WIDTH = 1280;
 
+//entities
  struct player{
-     int x, y;
+     float x, y;
+     float vx, vy; //velocity
+     float width, height;
      Color color;
-     int speed;
-     bool canMove;
+     float speed;
  };
 
  struct colider{
-     int x, y;
+     float x, y;
      Color color;
-     int width, height;
+     float width, height;
  };
+
+int solveColision(player &p, colider &c) //0 means no col solved, 1 means top col solved, 2 right, 3 bottom, 4 left
+{
+
+     if(p.x < c.x + c.width && p.x + p.width > c.x && p.y < c.y + c.height && p.y + p.height > c.y)
+     {
+         float left = (p.x + p.width) - c.x;
+         float right = (c.x + c.width) - p.x;
+         float top = (p.y + p.height) - c.y;
+         float bottom = (c.y + c.height) - p.y;
+
+         float minOverlapX = fmin(left, right);
+         float minOverlapY = fmin(top,bottom);
+         if(minOverlapX < minOverlapY)
+         {
+            if(left < right)
+            {
+                 p.x -= left;
+                 return 4;
+            }
+            else
+            {
+                 p.x += right;
+                 return 2;
+            }
+         }
+         else
+         {
+            if (top < bottom)
+            {
+                p.y -= top;
+
+                return 1;
+            }
+            else
+            {
+                 p.y += bottom;
+                 return 3;
+            }
+         }
+
+     }
+     return 0;
+}
 
 int main() {
     player player;
     player.x = 200;
     player.y = 200;
+    player.width = 50;
+    player.height = 50;
     player.color = {255, 0 , 0, 255};
     player.speed = 8;
-    player.canMove = true; //this definitly wont cause problems in the future
+
 
     colider colider;
     colider.x = 400;
     colider.y = 200;
-    colider.height = 800;
+    colider.height = 200;
     colider.width = 80;
     colider.color = BLACK;
-    bool isColiding = false;
-    int speedChangeX = 0;
-    int speedChangeY = 0;
+
     InitWindow(WIDTH,HEIGHT,"work");
     SetTargetFPS(30); //as of the shitty new version, no delta time so fps changes game speed
+    rlImGuiSetup(true);
+    int result;
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(WHITE); //any bg is better than onne
-
-        speedChangeX = 0;
-        speedChangeY = 0;
-        //draw player and coliderr
-        DrawRectangle(player.x,player.y,50,50, player.color);
-        DrawRectangle(colider.x, colider.y, colider.width, colider.height, colider.color);
-
-        if(player.canMove)
-        {
-        //speed
         if (IsKeyDown(KEY_LEFT))
         {
-            speedChangeX -= player.speed;
+            player.vx -= player.speed;
         }
         if (IsKeyDown(KEY_RIGHT))
         {
-            speedChangeX += player.speed;
+            player.vx += player.speed;
         }
         if (IsKeyDown(KEY_UP))
         {
-            speedChangeY -= player.speed;
+            player.vy -= player.speed;
         }
         if (IsKeyDown(KEY_DOWN))
         {
-            speedChangeY += player.speed;
+            player.vy += player.speed;
         }
-        }
 
+        player.x += player.vx;
+        player.y += player.vy;
 
+        player.vx = 0;
+        player.vy = 0;
 
+        result = solveColision(player, colider);
 
-        printf("x: %d y: %d\n", player.x, player.y);
-        printf("isColding: %d\n", isColiding);
-        player.x += speedChangeX;
-        player.y += speedChangeY;
-        isColiding = player.x < colider.x + colider.width && player.x + 50 > colider.x && player.y < colider.y + colider.height && player.y + 50 > colider.y;
-        while(isColiding)
-        {
-            speedChangeX = -speedChangeX;
-            speedChangeY = -speedChangeY;
-            while(isColiding){
-            player.x += speedChangeX ;
-            player.y += speedChangeY;
-            isColiding = player.x < colider.x + colider.width && player.x + 50 > colider.x && player.y < colider.y + colider.height && player.y + 50 > colider.y;
-            }
-            //"ehh, good enough" - mediocrates
-        }
+        rlImGuiBegin();
+        ImGui::Begin("cunt");
+        ImGui::Text("x: %f", player.x);
+        ImGui::Text("y: %f", player.y);
+        ImGui::Text("speed: %f", player.vx);
+        ImGui::Text("result: %d", result);
+        ImGui::End();
+        rlImGuiEnd();
+
+        DrawRectangle(player.x,player.y,player.width,player.height, player.color);
+        DrawRectangle(colider.x, colider.y, colider.width, colider.height, colider.color);
         EndDrawing();
-
     }
 
     printf("ay");
